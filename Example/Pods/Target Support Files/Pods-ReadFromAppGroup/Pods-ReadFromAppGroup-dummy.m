48cab8960e13cbe7ae388a3393845732a51e96fe
mzp <mzpppp@gmail.com>
2016-11-07 08:22:06 +0900

    :shell: bundle exec pod install

---
+++ Example/Pods/Target Support Files/Pods-ReadFromAppGroup/Pods-ReadFromAppGroup-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_ReadFromAppGroup : NSObject
+@end
+@implementation PodsDummy_Pods_ReadFromAppGroup
+@end
---
+++ Example/Pods/Target Support Files/Pods-WriteToAppGroup/Pods-WriteToAppGroup-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_WriteToAppGroup : NSObject
+@end
+@implementation PodsDummy_Pods_WriteToAppGroup
+@end
